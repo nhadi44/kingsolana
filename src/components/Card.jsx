2e4cdf11690073d2b253a47f0423122c6df828ef
nhadi44 <hadinurhidayat97@hotmail.com>
2022-03-09 04:58:24 +0700

    update page presale

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,60 @@
-import { Gallery } from './Gallery';
+import { Gallery } from "./Gallery";
 
-import image_01 from '../assets/images/image-01.jpg';
-import image_02 from '../assets/images/image-02.jpg';
-import image_03 from '../assets/images/image-03.jpg';
+import image_01 from "../assets/images/image-01.jpg";
+import image_02 from "../assets/images/image-02.jpg";
+import image_03 from "../assets/images/image-03.jpg";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
-export const Card = () => {
-	return (
-		<div>
-			<Gallery>
-				<div>
-					<img src={image_01} alt="nft-presale" className="w-1/3" />
-				</div>
-			</Gallery>
-		</div>
-	);
+
+export const Card = (props) => {
+  return (
+    <div className="relative">
+      <Gallery>
+        <div className="flex justify-center">
+          <div className="group w-1/6 mx-4">
+            <AnimationOnScroll animateIn="animate__flipInX">
+              <img
+                src={image_01}
+                alt="nft-presale"
+                className="blur-sm group-hover:blur-0 group-hover:scale-110 transition duration-[1s]"
+              />
+            </AnimationOnScroll>
+            <div className=" w-full text-center absolute left-0 opacity-0 translate-y-10 group-hover:opacity-100  group-hover:translate-y-10 transition  duration-[0.8s] ease-out">
+              <h1 className="text-white text-[50px] font-semibold uppercase">King Solana</h1>
+              <p className="text-white px-20 text-[30px] container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam neque, reprehenderit quod possimus asperiores quam harum officia fugiat rerum eveniet molestias ex assumenda modi adipisci laborum illum consectetur, facere ea?</p>
+            </div>
+          </div>
+          <div className="group w-1/6 mx-4">
+            <AnimationOnScroll animateIn="animate__flipInX animate__delay-1s">
+              <img
+                src={image_02}
+                alt="nft-presale"
+                className="blur-sm group-hover:blur-0 group-hover:scale-110 transition duration-[1s]"
+              />
+            </AnimationOnScroll>
+            <div className=" w-full text-center absolute left-0 opacity-0 translate-y-10 group-hover:opacity-100  group-hover:translate-y-10 transition  duration-[0.8s] ease-out">
+              <h1 className="text-white text-[50px] font-semibold uppercase">King Solana</h1>
+              <p className="text-white px-20 text-[30px] container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam neque, reprehenderit quod possimus asperiores quam harum officia fugiat rerum eveniet molestias ex assumenda modi adipisci laborum illum consectetur, facere ea?</p>
+            </div>
+          </div>
+          <div className="group w-1/6 mx-4">
+            <AnimationOnScroll animateIn="animate__flipInX animate__delay-2s">
+              <img
+                src={image_03}
+                alt="nft-presale"
+                className="blur-sm group-hover:blur-0 group-hover:scale-110 transition duration-[1s]"
+              />
+            </AnimationOnScroll>
+            <div className=" w-full text-center absolute left-0 opacity-0 translate-y-10 group-hover:opacity-100  group-hover:translate-y-10 transition  duration-[0.8s] ease-out">
+              <h1 className="text-white text-[50px] font-semibold uppercase">King Solana</h1>
+              <p className="text-white px-20 text-[30px] container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam neque, reprehenderit quod possimus asperiores quam harum officia fugiat rerum eveniet molestias ex assumenda modi adipisci laborum illum consectetur, facere ea?</p>
+            </div>
+          </div>
+        </div>
+        {/* <div className="flex justify-center">
+          <button className="bg-red-800 py-5 px-7 uppercase cursor-pointer text-white rounded-md translate-y-20 text-[30px] hover:bg-red-900">view project</button>
+        </div> */}
+      </Gallery >
+    </div >
+  );
 };
--- src/pages/Presale.jsx
+++ src/pages/Presale.jsx
@@ -5,7 +5,7 @@ export const Presale = () => {
 	return (
 		<div>
 			<Layout>
-				<div className="min-h-screen flex items-center justify-center">
+				<div className="min-h-screen flex translate-y-40 justify-center">
 					<Card />
 				</div>
 			</Layout>
